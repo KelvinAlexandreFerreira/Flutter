de4ecb25dfe7d7d5f1b64e569cf01d56d988301e
Kelvin Alexandre Ferreira <alexandre_kelvin@hotmail.com>
2023-08-01 19:45:26 -0400

    01 - Projeto base e métodos HTTP - Removendo uma entrada com DELETE

--- flutter_webapi_first_course-main/lib/screens/home_screen/widgets/journal_card.dart
+++ flutter_webapi_first_course-main/lib/screens/home_screen/widgets/journal_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_webapi_first_course/helpers/weekday.dart';
 import 'package:flutter_webapi_first_course/models/journal.dart';
+import 'package:flutter_webapi_first_course/services/journal_service.dart';
 import 'package:uuid/uuid.dart';
 
 class JournalCard extends StatelessWidget {
@@ -81,6 +82,11 @@ class JournalCard extends StatelessWidget {
                   ),
                 ),
               ),
+              IconButton(
+                  onPressed: () {
+                    removeJournal(context);
+                  },
+                  icon: const Icon(Icons.delete))
             ],
           ),
         ),
@@ -124,7 +130,7 @@ class JournalCard extends StatelessWidget {
     Navigator.pushNamed(
       context,
       'add-journal',
-      arguments: innerJournal,
+      arguments: map,
     ).then((value) {
       refreshFunction();
       if (value != null && value == true) {
@@ -141,5 +147,22 @@ class JournalCard extends StatelessWidget {
         );
       }
     });
+  }
+
+  removeJournal(BuildContext context) {
+    JournalService service = JournalService();
+    if (journal != null) {
+      service.delete(journal!.id).then((value) {
+        if (value) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text("Removido com sucesso!"),
+            ),
+          );
+
+          refreshFunction();
+        }
+      });
+    }
   }
 }
