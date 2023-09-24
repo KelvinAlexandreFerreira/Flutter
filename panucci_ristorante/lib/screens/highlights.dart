a7ed74a430211ab950c59ac8cc8c3357192c073d
Kelvin Alexandre Ferreira <alexandre_kelvin@hotmail.com>
2023-09-24 12:58:32 -0400

    05 - Fazer o App Responsivo na Horizontal - Consertando a página de destaques

--- panucci_ristorante/lib/screens/highlights.dart
+++ panucci_ristorante/lib/screens/highlights.dart
@@ -16,17 +16,67 @@ class Highlights extends StatelessWidget {
           const PageTitle(
             title: 'Destaques',
           ),
-          SliverList(
-            delegate: SliverChildBuilderDelegate((context, index) {
-              return HighlightItem(
-                imageURI: items[index]['image'],
-                itemTitle: items[index]['name'],
-                itemPrice: items[index]['price'],
-                itemDescription: items[index]['description'],
-              );
-            }, childCount: items.length),
-          ),
+          MediaQuery.of(context).orientation == Orientation.landscape
+              ? _landscapeList(items: items)
+              : _portraitList(items: items)
         ],
+      ),
+    );
+  }
+}
+
+// ignore: camel_case_types
+class _portraitList extends StatelessWidget {
+  const _portraitList({
+    super.key,
+    required this.items,
+  });
+
+  final List items;
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverList(
+      delegate: SliverChildBuilderDelegate((context, index) {
+        return HighlightItem(
+          imageURI: items[index]['image'],
+          itemTitle: items[index]['name'],
+          itemPrice: items[index]['price'],
+          itemDescription: items[index]['description'],
+        );
+      }, childCount: items.length),
+    );
+  }
+}
+
+// ignore: camel_case_types
+class _landscapeList extends StatelessWidget {
+  const _landscapeList({
+    super.key,
+    required this.items,
+  });
+
+  final List items;
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverGrid(
+      delegate: SliverChildBuilderDelegate(
+        (context, index) {
+          return HighlightItem(
+            imageURI: items[index]['image'],
+            itemTitle: items[index]['name'],
+            itemPrice: items[index]['price'],
+            itemDescription: items[index]['description'],
+          );
+        },
+        childCount: items.length,
+      ),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 8,
+        mainAxisSpacing: 8,
+        childAspectRatio: 0.9,
       ),
     );
   }
