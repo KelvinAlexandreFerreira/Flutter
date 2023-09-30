d7b54042296c03588c855d467271284e01468d2b
Kelvin Alexandre Ferreira <alexandre_kelvin@hotmail.com>
2023-09-29 21:23:29 -0400

    03 - Realizando Testes de Widget - Entendendo os Matchers

--- alubank_curso_testes/test/home_test.dart
+++ alubank_curso_testes/test/home_test.dart
@@ -1,3 +1,4 @@
+import 'package:estilizacao_componentes/components/box_card.dart';
 import 'package:estilizacao_componentes/data/bank_inherited.dart';
 import 'package:estilizacao_componentes/screens/home.dart';
 import 'package:flutter/material.dart';
@@ -44,4 +45,21 @@ void main() {
       expect(find.byKey(const Key('textKey')), findsOneWidget);
     },
   );
+
+  testWidgets('Finds 5 BoxCards', (tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: BankInherited(
+          child: Home(),
+        ),
+      ),
+    );
+    expect(find.byWidgetPredicate((widget) {
+      if (widget is BoxCard) {
+        return true;
+      } else {
+        return false;
+      }
+    }), findsNWidgets(5));
+  });
 }
