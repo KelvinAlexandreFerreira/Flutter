0785ff6eb9733a84004afa6a426f7f9b29b44ace
Kelvin Alexandre Ferreira <alexandre_kelvin@hotmail.com>
2023-10-03 05:53:53 -0400

    03 - Consumindo Dados - Aplicando o widget Consumer

--- alura_flutter_client_control-main/lib/main.dart
+++ alura_flutter_client_control-main/lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:client_control/models/client.dart';
+import 'package:client_control/models/client_type.dart';
 import 'package:client_control/models/clients.dart';
 import 'package:client_control/pages/client_types_page.dart';
 import 'package:flutter/material.dart';
@@ -6,7 +8,13 @@ import 'package:provider/provider.dart';
 
 void main() {
   runApp(ChangeNotifierProvider(
-      create: (context) => Clients(clients: []), child: const MyApp()));
+      create: (context) => Clients(clients: [
+            Client(
+                name: 'Geraldo',
+                email: 'leo@email.com',
+                type: ClientType(name: 'Platinum', icon: Icons.credit_card)),
+          ]),
+      child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
